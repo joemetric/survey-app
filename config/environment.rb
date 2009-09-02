34ed8b007bfe601ee6ea02591ab947217d6eb0dd
Naveen Joshi <naveenjoshi@mac.com>
2009-09-02 19:32:05 -0300

    unpacking gems, removing old ones ;)

--- config/environment.rb
+++ config/environment.rb
@@ -2,15 +2,15 @@ RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'rspec', :lib => false
-  config.gem 'rspec-rails', :lib => false
-  config.gem 'jscruggs-metric_fu', :lib => 'metric_fu', :source => 'http://gems.github.com'
+  config.gem 'rspec', :lib => false, :version => '>= 1.2.0'
+  config.gem 'rspec-rails', :lib => false, :version => '>= 1.2.0'
+  config.gem 'jscruggs-metric_fu', :lib => 'metric_fu', :version => '>= 1.1.4', :source => 'http://gems.github.com'
   config.gem 'giraffesoft-resource_controller', :lib => 'resource_controller', :source => 'http://gems.github.com'
   config.gem 'authlogic'
 
   # Let's try Remarkable instead of Shoulda.
   # Should be better
-  config.gem "carlosbrando-remarkable",  :lib => "remarkable", :source => "http://gems.github.com"
+  config.gem "carlosbrando-remarkable", :lib => "remarkable", :source => "http://gems.github.com"
   # config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com'
   
   
