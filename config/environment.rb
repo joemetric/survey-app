b74db1f37fb28ade65502f33ea592f6f71c3060b
Naveen Joshi <naveenjoshi@mac.com>
2009-09-02 18:55:27 -0300

    using remarkable instead of shoulda, and define gem dependency

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,15 @@ RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'rspec', :lib => false, :version => '>= 1.2.0'
+  config.gem 'rspec-rails', :lib => false, :version => '>= 1.2.0'
   config.gem 'jscruggs-metric_fu', :version => '1.1.5', :lib => 'metric_fu', :source => 'http://gems.github.com'
-  config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com'
+
+  # Let's try Remarkable instead of Shoulda.
+  # Should be better
+  config.gem "carlosbrando-remarkable",  :lib => "remarkable", :source => "http://gems.github.com"
+  # config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com'
+  
   
   config.load_paths += %W( #{RAILS_ROOT}/app/modules )
 
