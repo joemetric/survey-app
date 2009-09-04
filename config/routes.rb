22b8632f0a8dcccbdada4ffc6ab6c44fbefbf39d
Naveen Joshi <naveenjoshi@mac.com>
2009-09-04 03:56:21 -0300

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,20 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :pictures
   map.resources :completions
   map.resources :answers
+  map.resources :surveys do |survey|
+    survey.resources :questions
+  end
+  map.resource :user_session
+  map.resources :users, :member => { :not_active => :get }
 
   map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
   map.login '/login', :controller => 'user_sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
-  #map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
-
-  map.resources :users, :member => { :not_active => :get }
   map.activation 'users/:id/activate/:key', :controller => "users", :action => "activate"
-  
-  map.resource :user_session
 
   map.root :controller => 'surveys'
 
-  map.resources :surveys do |survey|
-    survey.resources :questions
-  end
 
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
