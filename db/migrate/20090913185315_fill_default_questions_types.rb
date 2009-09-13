21662569725aff62bc0a30fab98071bfc2968cc5
Naveen Joshi <naveenjoshi@mac.com>
2009-09-13 16:24:08 -0300

    adding wierd behaviour comment on migration

--- db/migrate/20090913185315_fill_default_questions_types.rb
+++ db/migrate/20090913185315_fill_default_questions_types.rb
@@ -1,5 +1,10 @@
 class FillDefaultQuestionsTypes < ActiveRecord::Migration
   def self.up
+    # Be Aware:
+    # When you run all the migrations for the first time, AR can't identify QuestionType as model, and it pressumes that create
+    # is a migration method and raises an exception.
+    # Running again without changing anything should solve the problem. 
+    # But this wierd behaviour must be fixed. I'm searching for an answer. 
     QuestionType.create({ :name => "Short Text Response", :field_type => "text_area" })
     QuestionType.create({ :name => "Multiple Choice", :field_type => "check_box" })
     QuestionType.create({ :name => "Photo Upload", :field_type => "file_field" })
