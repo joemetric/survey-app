3a05e520252342346f02e032ad65f424617a25e6
Naveen Joshi <naveenjoshi@mac.com>
2009-10-16 07:17:21 -0300

    changing string to text in survey description

---
+++ db/migrate/20091016100439_string_to_text_in_survey_description.rb
@@ -0,0 +1,11 @@
+class StringToTextInSurveyDescription < ActiveRecord::Migration
+  def self.up
+    remove_column :surveys, :description, :string
+    add_column :surveys, :description, :text
+  end
+
+  def self.down
+    remove_column :surveys, :description
+    add_column :surveys, :description, :string
+  end
+end
