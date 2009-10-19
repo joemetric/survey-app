0e13628ce5f80595ea4eac4187403a657bfbe14d
naveen.joshi <naveenjoshi@mac.com>
2009-10-20 02:18:07 +0800

    minor changes

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,31 +2,32 @@ class AnswersController < ApplicationController
   before_filter :require_user
   before_filter :find_question
   before_filter :find_or_create_reply
-  
+  skip_before_filter :verify_authenticity_token, :only => [:create]
+
   def new
     @answer = Answer.new
   end
-  
+
   def create
     if @question and @reply
       @answer = Answer.new params[:answer]
       @answer.reply, @answer.question = @reply, @question
-      response, header = @answer.save ? [@reply.to_json(:include => :answers), 201] : [@answer.errors.to_json, 422]
-      render :json => response, :header => header
+      response, header = @answer.save ? [@answer.to_json, 201] : [@answer.errors.to_json, 422]
+      render :json => response, :status => header
     else
-      render :json => nil, :header => 404
+      render :json => nil, :status => 404
     end
   end
-  
+
   private
-  
+
   def find_question
     @question = Question.find params[:question_id]
   end
-  
+
   def find_or_create_reply
     parameters = { :user_id => @current_user.id, :survey_id => @question.survey.id }
-    @reply = Reply.find(:first, :conditions => parameters) || Reply.create(parameters) 
+    @reply = Reply.find(:first, :conditions => parameters) || Reply.create(parameters)
   end
 
 end
