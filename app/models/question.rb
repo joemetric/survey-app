336753a00962b39657fb352eafec2d26de2a0b2f
Naveen Joshi <naveenjoshi@mac.com>
2009-11-11 10:46:24 -0200

    adding json reward available

--- app/models/question.rb
+++ app/models/question.rb
@@ -23,7 +23,7 @@ class Question < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :survey_id
 
-  attr_accessor :answer_by_user
+  attr_accessor :answer_by_user, :reward_available
 
   def options=(options_attributes)
     self.complement = options_attributes.split(",")
@@ -45,7 +45,8 @@ class Question < ActiveRecord::Base
 
   def to_json(options = {})
     self.answer_by_user = (survey.replies.by_user(options[:user]).first.answers.by_question(self).first rescue nil) if options[:user]
-    options.merge!(:methods => [:question_type_name, :answer_by_user])
+    self.reward_available = survey.payouts.send(package_question_type).amount # survey.payouts.send(package_question_type).amount
+    options.merge!(:methods => [:question_type_name, :answer_by_user, :reward_available])
     super
   end
 end
