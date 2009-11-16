89a46c2f8c1b71c6990168ea230ce46b9934e137
naveen.joshi <naveenjoshi@mac.com>
2009-11-16 14:51:19 +0800

    fix the bug of answer file_validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -16,31 +16,31 @@
 #
 
 class Answer < ActiveRecord::Base
-  
+
   has_attached_file :image
-  
+
   belongs_to :reply
   belongs_to :question
-  
+
   validates_uniqueness_of :question_id, :scope => "reply_id"
-  validates_presence_of :answer  
+  validates_presence_of :answer
   validates_attachment_content_type :image, :content_type => [ "image/gif", "image/jpeg", "image/png" ]
   validate :presence_of_file_only_if_allowed
-  
+
   named_scope :by_question, lambda { |q| { :conditions => { :question_id => q.id }}}
-  
+
   def reward # Reward for answering each question
     question.survey.payouts.send(question.package_question_type).amount
   end
-    
+
   private
-  
+
   def presence_of_file_only_if_allowed
     if QuestionType.accept_files.include?(question.question_type)
       errors.add(:file, "is required") if image_file_name.blank?
     else
-      errors.add(:file, "can't be accepted") if image_file_name.exists?
+      errors.add(:file, "can't be accepted") if image_file_name && image_file_name.exists?
     end
   end
-    
+
 end
