2789164ba5744edbd2c4504b2a9a6b5a80c6146c
Piyush Patil <piyush@allerin.com>
2009-11-23 18:29:50 +0530

    Removed unwanted files from spec/

--- spec/controllers/pictures_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-
-describe PicturesController do
-  # Replace this with your real tests.
-  it "should be true" do
-    true
-  end
-end
