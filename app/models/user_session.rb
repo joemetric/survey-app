189cee54f511b43040f7789bd5bdd7a62030d08f
Piyush Patil <piyush@allerin.com>
2009-11-24 13:05:48 +0530

    Fixed #624: iphone login: Better error message needed

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -2,6 +2,14 @@ class UserSession < Authlogic::Session::Base
   
   validate :under_maintenance
   
+  def validate_magic_states
+    return true if attempted_record.nil?
+    if attempted_record.respond_to?(:active?) && !attempted_record.active?
+      errors.add_to_base("A validation email has been sent to your email account. Please use that to activate your JoeSurvey account.")
+      return false
+    end
+  end  
+  
   private
   
   def under_maintenance
