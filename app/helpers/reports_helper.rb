096e004fc70e4e49cdfebc273bcb44b99d569ebc
Piyush Patil <piyush@allerin.com>
2009-12-04 18:21:43 +0530

    Fixed Tickets: #872, #873, #876, #877, #878

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -6,4 +6,22 @@ module ReportsHelper
     counter
   end
   
+  def photo_responses(answers)
+    rows = ''
+    td = ''
+    answers.each_with_index do |a, i|
+      i += 1
+      td += content_tag(:td, link_to_photo(a), :style => "text-align:left; text-indent:5px")
+      if i % 4 == 0
+        rows += content_tag(:tr, td)
+        td = ''
+      end
+    end
+    rows
+  end
+  
+  def link_to_photo(reponse)
+    link_to image_tag(reponse.image_url, :width => 119, :height => 119), reponse.image_url, :title => reponse.answer
+  end
+  
 end
