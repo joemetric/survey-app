5ff80ef61baf8ca29b75d85c0afb60b4c237d317
Adam Winter <adam@joemetric.com>
2010-01-22 17:13:17 -0500

    upgrading rspec to 1.3.0 and cucumber to 0.6.2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,3 +23,5 @@ config.action_mailer.delivery_method = :test
 
 # Configure App Address
 HOST = "localhost"
+
+  config.gem 'rspec-rails', :version => '>= 1.3.2', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
