16b70af5d1b32aa600913b59182f9a278c5498f0
GniYes Dev2 <dev2@gniyes.com>
2010-01-30 14:35:00 +0530

    commited the latest modified files

--- app/helpers/admin/charityorgs_helper.rb
+++ app/helpers/admin/charityorgs_helper.rb
@@ -1,2 +1,8 @@
 module Admin::CharityorgsHelper
+  
+  def file_name(file_path)
+    fileName = File.basename(file_path)
+    return fileName 
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ ActionController::Routing::Routes.draw do |map|
       :member => {:reset_password => :any, :change_type => :post},
       :collection => {:blacklist => :put}
     admin.resources :maintenances
-    admin.resources :charityorgs, :collection => { :create => :get, :updateOrganization => :put, :editOrganization => :put }
+    admin.resources :charityorgs, :collection => { :create => :get, :updateOrganization => :put, :editOrganization => :put, :attachFiles => :get }
   end
 
   map.resources :dashboard, :path_prefix => 'survey', :controller => "admin/dashboards",
