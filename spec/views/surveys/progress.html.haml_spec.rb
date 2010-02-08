a25633d3183a417e61f91fead97e1eef8c49781a
Kevin Farst <kfarst@kevin-david-farsts-macbook-2.local>
2010-02-08 14:23:39 -0500

    testing runcoderun fixes

--- spec/views/surveys/progress.html.haml_spec.rb
+++ spec/views/surveys/progress.html.haml_spec.rb
@@ -27,7 +27,7 @@ describe "/surveys/progress" do
   it "should display survey progress graph in span" do
     assigns[:survey] = assigns[:current_user].created_surveys.first
     render "/surveys/progress"
-    response.should have_tag("span#graph_3") 
+    response.should have_tag("span#graph_#{assigns[:survey].id}") 
   end 
   
   it "should display suvey questions in tabular format" do
