8290692c5a39040a09643cf0cab9a428f769ce12
Kevin Farst <kfarst@kevin-david-farsts-macbook-2.local>
2010-02-12 16:07:30 -0500

    some cucumber feature fixes

--- spec/factories/survey.rb
+++ spec/factories/survey.rb
@@ -5,5 +5,6 @@ Factory.define :survey do |s|
   s.payment_status 'incomplete'
   s.package_id 1
   s.association :payment, :factory => :payment
+  s.end_at "#{Date.today}"
   #s.question {|question| question.association(:question, :name => "Question Name")}
 end
