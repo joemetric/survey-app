24283f0b2d892f06a49e8d6eae54b52833973e03
Kevin Farst <kfarst@kevin-david-farsts-macbook-2.local>
2010-02-19 11:51:59 -0500

    removed paypal gateway information from environment.rb file and allowed the payout to be 0.0 for answers

--- config/environment.rb
+++ config/environment.rb
@@ -44,12 +44,5 @@ Rails::Initializer.run do |config|
   :password => 'odd71;humble',
   :authentication => :plain
   }
-  
-  config.after_initialize do 
-    ActiveMerchant::Billing::Base.gateway_mode = :test
-    GATEWAY = ActiveMerchant::Billing::Base.gateway(:paypal_express).new(:login => "dev_1252207951_biz_api1.joemetric.com", 
-                                                                         :password => "QY557JKYHNW5D5ZQ",
-                                                                         :signature => "AbmqUIwrIt8wU2gVWKdQdoCuN1bWAb9FQERQS0OSCvpgJou-rCTkYq54")
-  end
 
 end
