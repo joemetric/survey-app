e805ff5c79f9a43ebcdd57707efbddf6f2dbca4e
GniYes Dev2 <dev2@gniyes.com>
2010-02-27 20:13:48 +0530

    fixed the issue with postgres andf mysql conflist for organization grouping

--- app/models/nonprofit_orgs_earning.rb
+++ app/models/nonprofit_orgs_earning.rb
@@ -24,15 +24,29 @@ class NonprofitOrgsEarning < ActiveRecord::Base
 
   cols = NonprofitOrgsEarning.column_names.collect {|c| "nonprofit_orgs_earnings.#{c}"}.join(",")
   
-  named_scope :today, { :select => "DISTINCT ON (nonprofit_orgs_earnings.nonprofit_org_id) #{cols}", :conditions => ['created_at BETWEEN ? AND ?', Time.now.beginning_of_day, Time.now.end_of_day], }
-  named_scope :this_week, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', Time.now.beginning_of_week, Time.now.end_of_week]}
-  named_scope :last_week, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', 1.week.ago.beginning_of_week, 1.week.ago.end_of_week]}
-  named_scope :this_month, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', Time.now.beginning_of_month, Time.now.end_of_month]}
-  named_scope :last_month, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', Time.now.last_month.beginning_of_month, Time.now.last_month.end_of_month]}
-  named_scope :this_quarter, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', Time.now.beginning_of_quarter, Time.now.end_of_quarter]}
-  named_scope :last_quarter, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', @last_quarter_stat, Time.now.beginning_of_quarter]}
-  named_scope :this_year, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', Time.now.beginning_of_year, Date.today.end_of_year]}
-  named_scope :last_year, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', Time.now.last_year.beginning_of_year, Time.now.last_year.end_of_year]}
+  if DB_CONFIG[ENV["RAILS_ENV"]]["adapter"] == "postgresql"
+    named_scope :today, { :select => "DISTINCT ON (nonprofit_orgs_earnings.nonprofit_org_id) #{cols}", :conditions => ['created_at BETWEEN ? AND ?', Time.now.beginning_of_day, Time.now.end_of_day]}
+    named_scope :this_week, { :select => "DISTINCT ON (nonprofit_orgs_earnings.nonprofit_org_id) #{cols}", :conditions => ['created_at BETWEEN ? AND ?', Time.now.beginning_of_week, Time.now.end_of_week]}
+    named_scope :last_week, { :select => "DISTINCT ON (nonprofit_orgs_earnings.nonprofit_org_id) #{cols}", :conditions => ['created_at BETWEEN ? AND ?', 1.week.ago.beginning_of_week, 1.week.ago.end_of_week]}
+    named_scope :this_month, { :select => "DISTINCT ON (nonprofit_orgs_earnings.nonprofit_org_id) #{cols}", :conditions => ['created_at BETWEEN ? AND ?', Time.now.beginning_of_month, Time.now.end_of_month]}
+    named_scope :last_month, { :select => "DISTINCT ON (nonprofit_orgs_earnings.nonprofit_org_id) #{cols}", :conditions => ['created_at BETWEEN ? AND ?', Time.now.last_month.beginning_of_month, Time.now.last_month.end_of_month]}
+    named_scope :this_quarter, { :select => "DISTINCT ON (nonprofit_orgs_earnings.nonprofit_org_id) #{cols}", :conditions => ['created_at BETWEEN ? AND ?', Time.now.beginning_of_quarter, Time.now.end_of_quarter]}
+    named_scope :last_quarter, { :select => "DISTINCT ON (nonprofit_orgs_earnings.nonprofit_org_id) #{cols}", :conditions => ['created_at BETWEEN ? AND ?', @last_quarter_stat, Time.now.beginning_of_quarter]}
+    named_scope :this_year, { :select => "DISTINCT ON (nonprofit_orgs_earnings.nonprofit_org_id) #{cols}", :conditions => ['created_at BETWEEN ? AND ?', Time.now.beginning_of_year, Date.today.end_of_year]}
+    named_scope :last_year, { :select => "DISTINCT ON (nonprofit_orgs_earnings.nonprofit_org_id) #{cols}", :conditions => ['created_at BETWEEN ? AND ?', Time.now.last_year.beginning_of_year, Time.now.last_year.end_of_year]}
+  else
+    named_scope :today, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', Time.now.beginning_of_day, Time.now.end_of_day]}
+    named_scope :this_week, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', Time.now.beginning_of_week, Time.now.end_of_week]}
+    named_scope :last_week, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', 1.week.ago.beginning_of_week, 1.week.ago.end_of_week]}
+    named_scope :this_month, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', Time.now.beginning_of_month, Time.now.end_of_month]}
+    named_scope :last_month, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', Time.now.last_month.beginning_of_month, Time.now.last_month.end_of_month]}
+    named_scope :this_quarter, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', Time.now.beginning_of_quarter, Time.now.end_of_quarter]}
+    named_scope :last_quarter, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', @last_quarter_stat, Time.now.beginning_of_quarter]}
+    named_scope :this_year, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', Time.now.beginning_of_year, Date.today.end_of_year]}
+    named_scope :last_year, { :group => :nonprofit_org_id, :conditions => ['created_at BETWEEN ? AND ?', Time.now.last_year.beginning_of_year, Time.now.last_year.end_of_year]}
+  end
+  
+  
   
   validates_presence_of :nonprofit_org_id
   validates_presence_of :survey_id
