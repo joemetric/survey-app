52c33ae192a16b1969cd68f57f1a316726c80a18
Kevin Farst <kevin@joemetric.com>
2010-03-18 13:46:42 -0400

    removed the marital status from the demographic restrictions

--- app/models/restriction.rb
+++ app/models/restriction.rb
@@ -13,7 +13,7 @@
 
 class Restriction < ActiveRecord::Base
   # :geographic_location and :marital_status removed for now
-  Kinds = [ :gender, :zipcode, :occupation, :race, :education, :income, :age, :marital_status ]
+  Kinds = [ :gender, :zipcode, :occupation, :race, :education, :income, :age ]
   
   belongs_to :survey
   
